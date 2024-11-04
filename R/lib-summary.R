c026e8bf4ea46bda1522f152f17db9b81773cf66
Kayleigh Gillespie <kayleigh.gillespie@dfo-mpo.gc.ca>
2024-11-04 12:24:19 -0800

    Correct lib-summary.R function

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -10,8 +10,17 @@
 #' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
-  pkg_tbl <- table(p,s[, "LibPath"])
+  pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library","n_packages")
   pkg_df #this is the return at the end of code lines above
+}
+
+
+lib_summary <- function() {
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("Library", "n_packages")
+  pkg_df
 }
