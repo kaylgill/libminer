75cef04fd9e4714b04641c9dc4184a7ab0bc0c90
Kayleigh Gillespie <kayleigh.gillespie@dfo-mpo.gc.ca>
2024-11-05 10:31:39 -0800

    Add sizes and param

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,18 +1,32 @@
 #' Provide Number of R Packages by Library
 #'
 #' Provide the number of R package by library in
-#' a data.frame
+#'  a data.frame
+#'
+#' @param sizes Should sizes of libraries be calculated. Default `FALSE`.
 #'
 #' @return a data.frame of R packages by library
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)) {
+    stop("'sizes' must be logical (TRUE or FALSE)")
+  }
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
-  names(pkg_df) <- c("Library","n_packages")
-  pkg_df #this is the return at the end of code lines above
-}
+  names(pkg_df) <- c("Library", "n_packages")
 
+  if (isTRUE(sizes)) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
+  pkg_df
+}
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -1,14 +1,26 @@
-test_that("lib_summary returns expected results", {
+test_that("lib_summary returns expected results with defaults", {
   result <- lib_summary()
 
   expect_s3_class(result, "data.frame")
-  expect_equal(names(result), c("Library","n_packages"))
-  expect_type(result$n_packages, "integer")
-  expect_equal(ncol(result),2)
+  expect_equal(ncol(result), 2)
   expect_gte(nrow(result), 1)
-  expect_type(result$Library,"character")
+  expect_equal(names(result), c("Library", "n_packages"))
+  expect_type(result$n_packages, "integer")
+  expect_type(result$Library, "character")
 })
 
-test_that("lib summary fails appropriately", {
-  expect_error(lib_summary("hello"), "unused argument")
+test_that("sizes works", {
+  result <- lib_summary(size = TRUE)
+  expect_s3_class(result, "data.frame")
+  expect_equal(ncol(result), 3)
+  expect_gte(nrow(result), 1)
+  expect_equal(names(result), c("Library", "n_packages", "lib_size"))
+  expect_type(result$n_packages, "integer")
+  expect_type(result$Library, "character")
+  expect_type(result$lib_size, "double")
+})
+
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("hello"), "must be logical")
 })
